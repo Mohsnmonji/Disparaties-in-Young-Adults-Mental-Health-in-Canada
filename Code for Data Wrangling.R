df2d989d1c734437c549c6ed646cae280465cdc3
mohsen monji <mohsen.monji@concordia.ca>
2024-09-18 08:47:53 -0400

    up

--- Code for Data Wrangling.R
+++ Code for Data Wrangling.R
@@ -5,7 +5,7 @@ library(sjPlot)
 
 # Loading raw data
 # This assumes that you downloaded the GSS-2017 data and renamed it to 'gss2017.csv' 
-# and placed it  in the main directory where you placed the downloaded R scripts.
+# and placed it  in the main directory where you placed the downloaded R scripts from this repository.
 
 gss_2017 <- read_csv("gss2017.csv")
 
